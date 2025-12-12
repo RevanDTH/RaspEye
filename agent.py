42481cf7d6d218cb8a7d0e573b2af29081fc9a35
RevanDTH <davidkrammer21@gmail.com>
2025-12-12 10:42:10 +0100

    agent is ready for testing

--- agent.py
+++ agent.py
@@ -1,23 +1,24 @@
 ## Code for the Agent that is running on the host system
 
-import requests
-import psutil
-import json
+from requests import post
+from psutil import cpu_percent,virtual_memory,disk_usage
+from json import dumps
+from time import sleep
 
-cpu_percent = psutil.cpu_percent(interval=1)
-memory = psutil.virtual_memory()
-total_memory = memory.total / (1024 * 1024)
-disk_usage = psutil.disk_usage("/")
+while True:
 
-payload_obj = {
-    "cpu_percent":cpu_percent,
-    "memory":total_memory,
-    "disk_usage":disk_usage
-}
+    cpu_percent = cpu_percent(interval=1)
+    memory = virtual_memory()
+    total_memory = memory.total / (1024 * 1024)
+    disk_usage = disk_usage("/")
 
-payload = json.dumps(payload_obj)
+    payload_obj = {
+        "cpu_percent":cpu_percent,
+        "memory":total_memory,
+        "disk_usage":disk_usage
+    }
 
-
-uri = "http://127.0.0.1:5050/update_metrics"
-
-response = requests.post(url=uri,data=payload)
+    payload = dumps(payload_obj)
+    uri = "http://127.0.0.1:5050/update_metrics"
+    response = post(url=uri,data=payload)
+    sleep(15)
