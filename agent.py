e6e899fdd344cf9d63a8bee6ec59c681853199e8
RevanDTH <davidkrammer21@gmail.com>
2025-12-12 10:22:14 +0100

    Implement metrics collection and update endpoint in the agent and backend

--- agent.py
+++ agent.py
@@ -1,1 +1,23 @@
 ## Code for the Agent that is running on the host system
+
+import requests
+import psutil
+import json
+
+cpu_percent = psutil.cpu_percent(interval=1)
+memory = psutil.virtual_memory()
+total_memory = memory.total / (1024 * 1024)
+disk_usage = psutil.disk_usage("/")
+
+payload_obj = {
+    "cpu_percent":cpu_percent,
+    "memory":total_memory,
+    "disk_usage":disk_usage
+}
+
+payload = json.dumps(payload_obj)
+
+
+uri = "http://127.0.0.1:5050/update_metrics"
+
+response = requests.post(url=uri,data=payload)
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask,request
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "Hello World!"
+payload_obj = None
+
+
+@app.route('/update_metrics', methods=["POST"])
+def update_metrics():
+	payload = request.data
+	global payload_obj
+	try:
+		payload_obj = json.loads(payload.decode('utf-8'))
+	except Exception:
+		payload_obj = None
+
+	if payload_obj:
+		return "success"
+	else:
+		return "false"
+	
+@app.route('/debug')
+def debug_return():
+	if isinstance(payload_obj, dict):
+		return str(payload_obj.get('memory', "No metrics found"))
+	else:
+		return "No metrics found"
 
 
 
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5050)
