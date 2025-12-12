7042f2d82a13c7c28721568e183aed3e177196f2
RevanDTH <davidkrammer21@gmail.com>
2025-12-12 21:55:17 +0100

    added venv support for setup script

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from os import system,getcwd,chmod
 ### Setup code that runs once
 print("Installing RaspEye on your device . . .")
 
+system("python3 -m venv .venv")
+system("source .venv/bin/activate")
+
 print("Installing dependencies . . .")
 system("pip install -r ./requirements.txt")
 
@@ -10,10 +13,12 @@ system_service = input("Do you want RaspEye to create a service for the agent? (
 
 match system_service.lower:
     case "y":
-        agent_location = getcwd() + "/agent.py"
+        agent_path = getcwd()
+        agent_location = agent_path + "/agent.py"
         shell_string = f"""
 #!/bin/bash
 
+source {agent_path}/.venv/bin/activate
 python {agent_location}
         """
 
