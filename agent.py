3a6da5f791f0b9d4de0699e5e4c78e6524298091
RevanDTH <davidkrammer21@gmail.com>
2025-12-14 11:17:37 +0100

    added first concept of the config file

--- agent.py
+++ agent.py
@@ -4,22 +4,37 @@ from requests import post
 from psutil import cpu_percent,virtual_memory,disk_usage
 from json import dumps
 from time import sleep
+from yaml import safe_load
+
+with open("config.yaml") as f:
+    config = safe_load(f)
+
 
 while True:
 
-    cpu_load = cpu_percent(interval=1)
-    memory_info = virtual_memory()
-    memory_percent = memory_info.percent
-    disk_info = disk_usage("/")
+    if config["agent"]["display_cpu"]: 
+        cpu_load = cpu_percent(interval=1)
+    else:
+        cpu_load = None
+
+    if config["agent"]["display_memory"]: 
+        memory_info = virtual_memory()
+        memory_percent = memory_info.percent
+    else:
+        memory_percent = None  
+        
+    if config["agent"]["display_disk"]: 
+        disk_info = disk_usage("/").percent
+    else:
+        disk_info = None
 
     payload_obj = {
         "cpu_percent": cpu_load,
         "memory": memory_percent,
-        "disk_percent": disk_info.percent
+        "disk_percent": disk_info
     }
 
     payload = dumps(payload_obj)
-    uri = "http://127.0.0.1:5050/api/metrics/update"
+    uri = config["agent"]["send_url"]
     response = post(url=uri, data=payload)
-    print("Metrics sent . . .")
     sleep(15)
